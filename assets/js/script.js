8a8e6e90abc7181b2fa0da926d4be4f1c1b9fce6
David Martinez Rodríguez <321davidmr123@gmail.com>
2023-07-31 19:38:38 -0500

    all

--- assets/js/script.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("scroll", function(){
-    let nav = document.getElementById("toid");
-    // let li = document.getElementById("it");
-    nav.classList.toggle("asd", window.scrollY < 1)
-    // li.classList.toggle("letras", window.scrollY > 40)
-  })
